990998ac2b222674751b4da26a2aa577524bbe50
m1ngxiao <5797329@qq.com>
2024-10-30 15:15:15 +0800

    ignore model

--- server/task/CeleryTasks.py
+++ server/task/CeleryTasks.py
@@ -46,6 +46,7 @@ def do_lock_gpu(gpu_memory_threshold=16,ex=100):
             logger.info(f'gpu:{gpu.id} ,free:{gpu.memoryFree}')
             if gpu.memoryFree > gpu_memory_threshold * 1024:
                 lock_name = f"{server_ip}:GPU{gpu.id}"
+                logger.info(f'do lock {lock_name}')
                 # 尝试获取锁
                 if redis_client.set(
                         lock_name, "1", nx=True, ex=ex
